76a69dec1b8300a6a7d40e7395899599e8a86021
balajikandan <kbalaji@collab.net>
2013-11-13 08:50:41 +0000

    Integrate code formatting changes in subversion
    Changes done:
    
    Integrating code style configuration for CCF-Integration project.
    
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk/CCF-Integration@5738 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- plugins/TFS/src/main/java/com/collabnet/ccf/ccfmaster/gp/tfs/validator/custom/TFSCustomRMDParticipant.java
+++ plugins/TFS/src/main/java/com/collabnet/ccf/ccfmaster/gp/tfs/validator/custom/TFSCustomRMDParticipant.java
@@ -6,28 +6,29 @@ import com.collabnet.ccf.ccfmaster.gp.web.model.RMDModel;
 import com.collabnet.ccf.ccfmaster.gp.web.rmd.DefaultCustomRMDParticipant;
 import com.collabnet.ccf.ccfmaster.server.domain.CCFCoreProperty;
 
-public class TFSCustomRMDParticipant extends DefaultCustomRMDParticipant{
+public class TFSCustomRMDParticipant extends DefaultCustomRMDParticipant {
 
-	@Override
-	public String getParticipantRepositoryId(RMDModel model) {
-		StringBuilder builder = new StringBuilder();
-		List<CCFCoreProperty> configProperties = model.getParticipantSelectorFieldList();
-		for (CCFCoreProperty property : configProperties) { 
-			String name = property.getName();
-			if(name.equalsIgnoreCase("tfsCollection")){
-				builder.append(property.getValue());
-				continue;
-			}
-			if(name.equalsIgnoreCase("tfsProjectList")){
-				builder.append("-").append(property.getValue());
-				continue;
-			}
-			if(name.equalsIgnoreCase("tfsWorkItemType")){
-				builder.append("-").append(property.getValue());
-				continue;
-			}
-		}
-		return builder.toString();
-	}
+    @Override
+    public String getParticipantRepositoryId(RMDModel model) {
+        StringBuilder builder = new StringBuilder();
+        List<CCFCoreProperty> configProperties = model
+                .getParticipantSelectorFieldList();
+        for (CCFCoreProperty property : configProperties) {
+            String name = property.getName();
+            if (name.equalsIgnoreCase("tfsCollection")) {
+                builder.append(property.getValue());
+                continue;
+            }
+            if (name.equalsIgnoreCase("tfsProjectList")) {
+                builder.append("-").append(property.getValue());
+                continue;
+            }
+            if (name.equalsIgnoreCase("tfsWorkItemType")) {
+                builder.append("-").append(property.getValue());
+                continue;
+            }
+        }
+        return builder.toString();
+    }
 
 }
--- plugins/TFS/src/main/java/com/collabnet/ccf/ccfmaster/gp/tfs/validator/custom/TFSGenericParticipantConfigValidator.java
+++ plugins/TFS/src/main/java/com/collabnet/ccf/ccfmaster/gp/tfs/validator/custom/TFSGenericParticipantConfigValidator.java
@@ -18,82 +18,96 @@ import com.microsoft.tfs.core.exceptions.TFSUnauthorizedException;
  * Sample TFS validator used to validate GenericParticipant attributes
  * 
  * @author kbalaji
- *
+ * 
  */
 public class TFSGenericParticipantConfigValidator implements IGenericParticipantConfigItemValidator {
 
-	private static final String CONFIG_ERROR_MSG = "Please check the configuration again";
-	private static final String CCF_PARTICIPANT_TFS_URL = "ccf.participant.tfs.url";
-	private static final String CCF_LANDSCAPE_TFS_PASSWORD = "ccf.landscape.tfs.password";
-	private static final String CCF_LANDSCAPE_TFS_USERNAME = "ccf.landscape.tfs.username";
-	
-	
-	public void validateValue(List<CCFCoreProperty> configProperties, Errors errors, String errorElementName) {		
-		for (int i = 0; i < configProperties.size(); i++) { 
-			String value = configProperties.get(i).getValue();
-			CCFCorePropertyType type = configProperties.get(i).getType();
-			String conditionalRegex = configProperties.get(i).getConditionalRegex();
-			if(StringUtils.isEmpty(value)){
-				errors.rejectValue(errorElementName+"[" + i + "].value", "Cannot be blank. Please enter a value","Blank value not accepted");
-			} else if(!ValidatorUtils.validateType( value, type )){
-				errors.rejectValue(errorElementName+"[" + i + "].value","","Value should be numeric"); // Numeric validation message will always be expected
-			} else if(!ValidatorUtils.findMatch(conditionalRegex, value)){
-				errors.rejectValue(errorElementName+"[" + i + "].value","","Value given is not matching conditional regex");
-			}
-		}
-	}
+    private static final String CONFIG_ERROR_MSG           = "Please check the configuration again";
+    private static final String CCF_PARTICIPANT_TFS_URL    = "ccf.participant.tfs.url";
+    private static final String CCF_LANDSCAPE_TFS_PASSWORD = "ccf.landscape.tfs.password";
+    private static final String CCF_LANDSCAPE_TFS_USERNAME = "ccf.landscape.tfs.username";
 
-	@Override
-	public ValidationResult validateConnection(AbstractGenericParticipantModel model) {
-		TFSTeamProjectCollection configurationServer = null;
-		String userName = null, domain = null, url = null, password = null;
-		List<CCFCoreProperty> landscapeConfigList = model.getLandscapeConfigList();
-		List<CCFCoreProperty> partcipantConfigList = model.getParticipantConfigList();
-		for(CCFCoreProperty property : landscapeConfigList ){
-			if(property.getName().equals(CCF_LANDSCAPE_TFS_USERNAME)){
-				String[] name = property.getValue().split("\\\\");
-				if(name.length >1){
-					domain = name[0];
-					userName = name[1];
-				}else {
-					userName = name[0];
-				}
-			}
-			if(property.getName().equals(CCF_LANDSCAPE_TFS_PASSWORD)){
-				password= property.getValue();
-			}
-		}
-		for(CCFCoreProperty property : partcipantConfigList ){
-			if(property.getName().equals(CCF_PARTICIPANT_TFS_URL)){
-				url = property.getValue();
-			}
-		}
-		
-		if(StringUtils.isEmpty(userName) || StringUtils.isEmpty(url)){
-			return new ValidationResult(false, CONFIG_ERROR_MSG);
-		}else {
-			try{
-				configurationServer = new TFSTeamProjectCollection(url,  userName, domain, password);
-				configurationServer.authenticate();
-				return new ValidationResult(configurationServer.hasAuthenticated());
-			} catch(TFSUnauthorizedException e){
-				return new ValidationResult(false, e.getMessage());
-			} catch(Exception e){
-				return new ValidationResult(false, e.getMessage());
-			}/*finally{
-				if(configurationServer != null)
-				configurationServer.close();
-			}*/
-		}
-	}
+    @Override
+    public void validate(AbstractGenericParticipantModel model, Errors errors) {
+        // NOTE: this implementation can be customized as per third party participant implementation
+        List<CCFCoreProperty> landscapeConfigList = model
+                .getLandscapeConfigList();
+        List<CCFCoreProperty> participantConfigList = model
+                .getParticipantConfigList();
+        validateValue(landscapeConfigList, errors,
+                LANDSCAPE_CONFIG_LIST_ELEMENT_NAME);
+        validateValue(participantConfigList, errors,
+                PARTICIPANT_CONFIG_LIST_ELEMENT_NAME);
+    }
 
-	@Override
-	public void validate(AbstractGenericParticipantModel model, Errors errors) {
-		// NOTE: this implementation can be customized as per third party participant implementation
-		List<CCFCoreProperty> landscapeConfigList = model.getLandscapeConfigList();
-		List<CCFCoreProperty> participantConfigList = model.getParticipantConfigList();
-		validateValue(landscapeConfigList, errors,LANDSCAPE_CONFIG_LIST_ELEMENT_NAME);
-		validateValue(participantConfigList, errors,PARTICIPANT_CONFIG_LIST_ELEMENT_NAME);
-	}
-	
+    @Override
+    public ValidationResult validateConnection(
+            AbstractGenericParticipantModel model) {
+        TFSTeamProjectCollection configurationServer = null;
+        String userName = null, domain = null, url = null, password = null;
+        List<CCFCoreProperty> landscapeConfigList = model
+                .getLandscapeConfigList();
+        List<CCFCoreProperty> partcipantConfigList = model
+                .getParticipantConfigList();
+        for (CCFCoreProperty property : landscapeConfigList) {
+            if (property.getName().equals(CCF_LANDSCAPE_TFS_USERNAME)) {
+                String[] name = property.getValue().split("\\\\");
+                if (name.length > 1) {
+                    domain = name[0];
+                    userName = name[1];
+                } else {
+                    userName = name[0];
+                }
+            }
+            if (property.getName().equals(CCF_LANDSCAPE_TFS_PASSWORD)) {
+                password = property.getValue();
+            }
+        }
+        for (CCFCoreProperty property : partcipantConfigList) {
+            if (property.getName().equals(CCF_PARTICIPANT_TFS_URL)) {
+                url = property.getValue();
+            }
+        }
+
+        if (StringUtils.isEmpty(userName) || StringUtils.isEmpty(url)) {
+            return new ValidationResult(false, CONFIG_ERROR_MSG);
+        } else {
+            try {
+                configurationServer = new TFSTeamProjectCollection(url,
+                        userName, domain, password);
+                configurationServer.authenticate();
+                return new ValidationResult(
+                        configurationServer.hasAuthenticated());
+            } catch (TFSUnauthorizedException e) {
+                return new ValidationResult(false, e.getMessage());
+            } catch (Exception e) {
+                return new ValidationResult(false, e.getMessage());
+            }/*
+              * finally{ if(configurationServer != null)
+              * configurationServer.close(); }
+              */
+        }
+    }
+
+    public void validateValue(List<CCFCoreProperty> configProperties,
+            Errors errors, String errorElementName) {
+        for (int i = 0; i < configProperties.size(); i++) {
+            String value = configProperties.get(i).getValue();
+            CCFCorePropertyType type = configProperties.get(i).getType();
+            String conditionalRegex = configProperties.get(i)
+                    .getConditionalRegex();
+            if (StringUtils.isEmpty(value)) {
+                errors.rejectValue(errorElementName + "[" + i + "].value",
+                        "Cannot be blank. Please enter a value",
+                        "Blank value not accepted");
+            } else if (!ValidatorUtils.validateType(value, type)) {
+                errors.rejectValue(errorElementName + "[" + i + "].value", "",
+                        "Value should be numeric"); // Numeric validation message will always be expected
+            } else if (!ValidatorUtils.findMatch(conditionalRegex, value)) {
+                errors.rejectValue(errorElementName + "[" + i + "].value", "",
+                        "Value given is not matching conditional regex");
+            }
+        }
+    }
+
 }
